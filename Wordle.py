bee373b200cd9d1c072f4bf8aa0afb2fe8dc2db6
Aqusely <kirillnalivajko96@gmail.com>
2023-08-11 15:37:38 +0500

    Функция game_logic

--- Wordle.py
+++ Wordle.py
@@ -22,12 +22,20 @@ def start_game(answer):     # Функция приветсвия
     
     if answer == 'да':
         print('Давайте начнём играть. У тебя есть 5 попыток чтобы угадать слово, загаданное мной. Буквы которые стоят на нужных местах подсвечиваются зеленым, если буквы есть в слове, но они не на своих местах, они подсвечиваются желтым.')
+        game_logic(input('Введите слово: '))
     else:
         print('Пока')
         
 
 def game_logic(your_word):
     words = list(str(your_word))   # рАзбиваем слово пользователя на буквы
-          
+    for i in range(len(your_word)):
+        if symbol_word[i] == word[i]:
+            print('Буква на своем месте: ')
+        elif word[i] in symbol_word:
+            print('Буква есть в слове, но не на своем месте')
+        else:
+            continue
 
+print(symbol_word)
 start_game(input('Готовы ли Вы начать играть?: '))
