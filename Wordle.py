83cef2d8fe979b3c3e7d439e23d0042a93fa3e1a
Aqusely <kirillnalivajko96@gmail.com>
2023-08-11 18:08:05 +0500

    Добавил цикл while
    -Добавил цикл While
    -Организовал подсчет попыток

--- Wordle.py
+++ Wordle.py
@@ -18,20 +18,29 @@ words_list = [     #Список слов
 
 word = random.choice(words_list)   # Случайный выбор слова 
 symbol_word = list(str(word))  # # Слово по буквам  
-final_word = []
+final_word = []  
 
 def start_game(answer):     # Функция приветсвия
     
+    
+
     if answer == 'да':
         print('Давайте начнём играть. У тебя есть 5 попыток чтобы угадать слово, загаданное мной. Буквы которые стоят на нужных местах подсвечиваются зеленым, если буквы есть в слове, но они не на своих местах, они подсвечиваются желтым.')
-        game_logic(input('Введите слово: '))
+        print('Введите слово: ')
+        
+        game_logic(input())
+            
     else:
         print('Пока')
         
 def game_logic(your_word):
 
-    words = list(str(your_word))   # разбиваем слово пользователя на буквы
-    if word != your_word:
+    count = 0
+
+    while count != 5 and your_word != word:
+
+        words = list(str(your_word))   # разбиваем слово пользователя на буквы
+        
         for i in range(len(your_word)):
             if symbol_word[i] == words[i]:
                 color(symbol=words[i], color='Green')
@@ -40,8 +49,21 @@ def game_logic(your_word):
             else:
                 color(symbol=words[i], color='White')
         print(''.join(final_word))
-    else:
-        print(f'Позравляем, вы победили. Это слово {your_word}')
+        print(Fore.WHITE)
+        final_word.clear()
+    
+        count += 1
+
+        if count == 5:
+            print('У вас кончились попытки. Попробуйте еще раз')
+            break
+        elif word == your_word:
+            print(f'Поздравляем, вы выиграли. Это слово {your_word}')
+            break
+
+        your_word = input()
+        
+    
 
 
 def color(symbol, color):
